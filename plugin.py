06eff42bff742f145fb0e52e36153d27a4160d68
Jason Huang <hhcnau@gmail.com>
2025-07-12 15:53:15 +0930

    Initial ocmmit

---
+++ plugin.py
@@ -0,0 +1,18 @@
+from inventree.plugins import InvenTreePlugin
+from inventree.plugins.base.template import PluginTemplateView
+from django.urls import path
+
+class MyUIView(PluginTemplateView):
+    template_name = "my_ui_plugin/index.html"
+    title = "Mobile UI"
+    icon = "fas fa-mobile-alt"
+
+class MyUIPlugin(InvenTreePlugin):
+    NAME = "MyUIPlugin"
+    TITLE = "Mobile UI"
+    SLUG = "my_ui_plugin"
+
+    def get_urls(self):
+        return [
+            path('ui/', MyUIView.as_view(), name='plugin-ui'),
+        ]

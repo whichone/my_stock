47f221273353b9653e3eaffd69f2bb6aacd272df
Jason Huang <hhcnau@gmail.com>
2025-07-13 13:50:52 +0930

    log

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
-print("Loading my_stock plugin module...")
-
+import os
+import logging
 from plugin import InvenTreePlugin
 from plugin.mixins.settings import SettingsMixin
+
+# Get the directory where this file (plugin.py) resides
+PLUGIN_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# Log file inside plugin folder
+log_path = os.path.join(PLUGIN_DIR, 'my_stock_plugin.log')
+
+logger = logging.getLogger('my_stock')
+logger.setLevel(logging.DEBUG)
+
+fh = logging.FileHandler(log_path)
+fh.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+
+logger.addHandler(fh)
 
 class MinimalPlugin(SettingsMixin, InvenTreePlugin):
     NAME = "my_stock"
@@ -19,4 +36,4 @@ class MinimalPlugin(SettingsMixin, InvenTreePlugin):
     }
 
     def setup(self):
-        print(">>> MinimalPlugin setup called")
+        logger.debug(">>> MinimalPlugin setup called")

5e89cab8a375abefb7c4fb06cc2ce9c77f96abca
Jason Huang <hhcnau@gmail.com>
2025-07-13 10:16:56 +0930

    update 1

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from plugin import InvenTreePlugin
 from plugin.mixins import SettingsMixin, PanelMixin
+from .settings import MyStockSettings
+from .views import MyStockReactView
 
 class MinimalPlugin(PanelMixin, SettingsMixin, InvenTreePlugin):
     NAME = "my_stock"
@@ -7,12 +9,8 @@ class MinimalPlugin(PanelMixin, SettingsMixin, InvenTreePlugin):
     DESCRIPTION = "A simple plugin that adds a custom admin panel"
     VERSION = "0.1"
 
-    def get_custom_panels(self, request):
-        return [
-            {
-                'title': 'Hello World Panel',
-                'icon': 'circle',  # Use any Tabler icon name
-                'content': '<p>This is a custom panel from the <strong>My Stock</strong>!</p>',
-                'enabled': True
-            }
-        ]
+    SETTINGS = MyStockSettings
+
+    def setup(self):
+        # Register the React view
+        self.add_view(MyStockReactView)

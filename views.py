100aac23de5b252eaa8d14e695795d6f4585501e
Jason Huang <hhcnau@gmail.com>
2025-07-13 13:13:30 +0930

    setting test

--- views.py
+++
@@ -1,8 +0,0 @@
-from plugin.views import PluginReactView
-
-
-class MyStockReactView(PluginReactView):
-    name = "my_stock_ui"
-    icon = "fas fa-mobile-alt"
-    title = "Mobile UI"
-    description = "A React panel for mobile-friendly stock management"

39d156d5237b5ce3790c63ca647592bac6bb8ca5
Jason Huang <hhcnau@gmail.com>
2025-07-13 13:32:00 +0930

    re-copy

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
 from plugin import InvenTreePlugin
 from plugin.mixins import SettingsMixin
-from django.utils.translation import gettext_lazy as _
-#from .settings import MyStockSettings
-#from .views import MyStockReactView
 
-
-class MyStock(SettingsMixin, InvenTreePlugin):
-#class MinimalPlugin(SettingsMixin, PluginConfigMixin, InvenTreePlugin):
+class MinimalPlugin(SettingsMixin, InvenTreePlugin):
     NAME = "my_stock"
     TITLE = "My Stock"
-    DESCRIPTION = "A simple plugin with settings and a React UI"
+    DESCRIPTION = "A simple plugin with settings"
     VERSION = "0.1"
 
     SETTINGS = {
@@ -19,9 +14,7 @@ class MyStock(SettingsMixin, InvenTreePlugin):
             'validator': bool,
             'default': True,
         }
-
     }
 
     def setup(self):
         print(">>> MinimalPlugin setup called")
-        #self.add_view(MyStockReactView)

978f83cb61c8d09f74aa5e66b12d36e4371e84c8
Jason Huang <hhcnau@gmail.com>
2025-07-13 21:08:19 +0930

    dddd

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from plugin import InvenTreePlugin
 from plugin.mixins import SettingsMixin, UserInterfaceMixin
 
-class MyPlugin(SettingsMixin, UserInterfaceMixin, InvenTreePlugin):
-    NAME = "MyStock"
+class MyStock(SettingsMixin, UserInterfaceMixin, InvenTreePlugin):
+    NAME = "my_stock"
     TITLE = "My Stock"
     DESCRIPTION = "A plugin with settings and a basic UI"
     VERSION = "0.5"

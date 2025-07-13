ab89a26ab7e5114978cfe18b87207a3b62d41d4a
Jason Huang <hhcnau@gmail.com>
2025-07-13 20:47:26 +0930

    d

--- views.py
+++ views.py
@@ -1,7 +1,7 @@
 from plugin.base.template import PluginTemplateView
-from plugin.mixins import PluginConfigMixin
+#from plugin.mixins import PluginConfigMixin
 
-class MyStockAdminView(PluginConfigMixin, PluginTemplateView):
+class MyStockAdminView(PluginTemplateView):
     template_name = "my_stock/index.html"
     view_name = "my_stock_panel"
     icon = "fa-boxes"
